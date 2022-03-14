41358f46534e8530adeb2587cd673431afc5be3d
abdulkerim-adem <abdulkerimadem36@gmail.com>
2022-03-14 08:44:51 -0700

    Intr to C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lowercase then in uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
