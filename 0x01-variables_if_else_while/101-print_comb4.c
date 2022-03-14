c41433dde74f18539e3288a9c978a83c9bef0d22
abdulkerim-adem <abdulkerimadem36@gmail.com>
2022-03-14 10:10:11 -0700

    Intr to C

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-	int i,j,k;
+	int i, j , k;
 
 	for (i = 48; i <= 55; i++)
 	{
-		for (j = i+1; j <= 56; j++)
+		for (j = i + 1; j <= 56; j++)
 		{
-			for (k = j+1; k <= 57; k++)
+			for (k = j + 1; k <= 57; k++)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if ( i != 5 || j != 55 || k != 56)
+				if (i != 55 || j != 56 || k != 57)
 				{
 					putchar(',');
 					putchar(' ');
