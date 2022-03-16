2d2bc3a22eff2aa409a945a967a47d0e3cfec415
abdulkerim-adem <abdulkerimadem36@gmail.com>
2022-03-16 04:25:52 -0700

    C functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase followed by a new line
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
