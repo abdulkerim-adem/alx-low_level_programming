3bf94f78d4ded0f0620fa0a6d76a55ca8050ce01
abdulkerim-adem <abdulkerimadem36@gmail.com>
2022-03-16 05:32:29 -0700

    C functions and nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
- * @n-the number generated randomly
+ * @n: the number generated randomly
  */
 void print_to_98(int n)
 {
