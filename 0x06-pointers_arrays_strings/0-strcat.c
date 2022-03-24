2e793965a2c30ea8ea2b5aa4cf08bd1353ba52db
abdulkerim-adem <abdulkerimadem36@gmail.com>
2022-03-24 09:22:10 -0700

    C Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - concatinate two strings
- *@dest: destination string
- *@src: source string to be copied
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
